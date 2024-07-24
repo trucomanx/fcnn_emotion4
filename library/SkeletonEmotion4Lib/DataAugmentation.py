8ff38692c54e72056cc168b34c3017b790cc5942
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-07-24 13:22:13 -0300

    ordenando

--- library/SkeletonEmotion4Lib/DataAugmentation.py
+++ library/SkeletonEmotion4Lib/DataAugmentation.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import numpy as np
 
 class DataAugmentationEncDecGenerator(tf.keras.utils.Sequence):
-    def __init__(self, data, batch_size=32, augment_fn=None):
+    def __init__(self, data, batch_size=32, augment_fn=None, shuffle=False):
         '''
         data: Matrix of type Numpy array
         batch_size: Batch size
@@ -13,6 +13,7 @@ class DataAugmentationEncDecGenerator(tf.keras.utils.Sequence):
         self.data = data
         self.batch_size = batch_size
         self.augment_fn = augment_fn
+        self.shuffle = shuffle
         self.indices = np.arange(len(self.data))
 
     def __len__(self):
@@ -28,29 +29,33 @@ class DataAugmentationEncDecGenerator(tf.keras.utils.Sequence):
         return batch_data, batch_data  # Encoder-decoder model
 
     def on_epoch_end(self):
-        np.random.shuffle(self.indices)
+        if self.shuffle:
+            np.random.shuffle(self.indices)
 
 
 
 from sklearn.preprocessing import LabelBinarizer
 
 class DataAugmentationClsGenerator(tf.keras.utils.Sequence):
-    def __init__(self, data, labels, batch_size=32, augment_fn=None):
+    def __init__(self, data, labels, batch_size=32, augment_fn=None, shuffle=False):
         '''
         data: Matrix of type Numpy array
-        labels: Vector com as labels
+        labels: Vector with the labels
         batch_size: Batch size
         augment_fn: Augmented function, receive a numpy array and return a numpy array of same shape.
+        shuffle: if shuffle on epoch end
         '''
         self.data = data
         self.labels = labels
         self.batch_size = batch_size
         self.augment_fn = augment_fn
+        self.shuffle = shuffle
         self.indices = np.arange(len(self.data))
         
         # One-hot encode the labels
         self.label_binarizer = LabelBinarizer()
-        self.one_hot_labels = self.label_binarizer.fit_transform(self.labels)
+        self.one_hot_labels = self.label_binarizer.fit_transform(self.labels);
+        self.classes = np.argmax(self.one_hot_labels, axis=1);
 
     def __len__(self):
         return int(np.ceil(len(self.data) / self.batch_size))
@@ -66,6 +71,7 @@ class DataAugmentationClsGenerator(tf.keras.utils.Sequence):
         return batch_data, batch_labels
 
     def on_epoch_end(self):
-        np.random.shuffle(self.indices)
+        if self.shuffle:
+            np.random.shuffle(self.indices);
 
 
