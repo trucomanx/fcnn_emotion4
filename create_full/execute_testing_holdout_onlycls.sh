020ba4d6a772a98b611dda578735ae094f7caca6
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-07-10 03:55:36 -0300

    ordenando

---
+++ create_full/execute_testing_holdout_onlycls.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+PyCommand='#!/usr/bin/python3
+
+json_filename="testing_data_results.json"
+
+model_list=["onlycls_ncod15",
+            "onlycls_ncod18",
+            "onlycls_ncod20",
+            "onlycls_ncod22",
+            "onlycls_ncod25"
+            ];
+
+info_list=[ "block_delayms",
+            "categorical_accuracy",
+            "loss",
+            "number_of_parameters"
+            ];
+
+sep=",";
+
+image_ext=".eps";
+'
+
+# HD
+BaseDir='/media/shannon/Expansion'
+#BaseDir='/mnt/8811f502-ae19-4dd8-8371-f1915178f581/Fernando'
+#BaseDir='/media/fernando/B0EA304AEA300EDA/Dados/Fernando'
+
+OutDir=$BaseDir'/OUTPUTS/DOCTORADO2/fcnn_emotion4_1_10times'
+
+DName='full2024-skel'
+
+
+InTsD=$BaseDir'/DATASET/TESE'
+InTsF='test_skeleton.csv'
+ModD=$BaseDir'/OUTPUTS/DOCTORADO2/SKEL/fcnn_emotion4_full/full2024-skel/training_validation_holdout'
+
+################################################################################
+
+mkdir -p $OutDir/$DName/testing_holdout
+echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/testing_holdout/'main.py'
+
+################################################################################
+
+ipynb-py-convert testing_holdout_onlycls.ipynb testing_holdout_onlycls.py
+
+for ncod in 15 18 20 22 25 29 33 37 41 45 49 53 57 61 65 69 73 77 81 85 89; do #15 18 20 22 25
+    echo " "
+    python3 testing_holdout_onlycls.py  --ncod $ncod \
+                                        --model-file $ModD/'onlycls_ncod'$ncod/'model_onlycls_ncod'$ncod'.h5' \
+                                        --times 10 \
+                                        --batch-size 16 \
+                                        --dataset-test-dir $InTsD \
+                                        --dataset-test-file $InTsF \
+                                        --dataset-name $DName \
+                                        --output-dir $OutDir
+done
+
+
+
+
+rm -f testing_holdout_onlycls.py
+
